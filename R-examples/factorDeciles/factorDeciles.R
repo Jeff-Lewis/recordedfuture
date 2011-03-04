e5817b6187e4f0d95143fe9385bda4a489fa8fe9
evan.sparks <evan.sparks@08715f25-0a8d-29aa-5c68-1888cdaa29f9>
2011-03-04 21:34:54 +0000

    Adding Factor Decile example code.

---
+++ R-examples/factorDeciles/factorDeciles.R
@@ -0,0 +1,100 @@
+library(fImport)
+library(fTrading)
+library(PerformanceAnalytics)
+library(reshape)
+
+source("rfapi.R")
+
+#Configure portfolio parameters here.
+tickerfile <- "sp500-tickers.txt" #Name of a file that contains the tickers I'm interested in.
+from <- "2009-01-01" #Start date.
+to <- "2011-03-01" #End date.
+token <- paste(Sys.getenv('RFTOKEN')) #My Recorded Future token.
+
+#Define a function which pulls market data and computes leading 1-day returns.
+pull_market_data <- function(tickerlist, from, to, doparallel=FALSE, nprocs=5) {
+
+	#This function pulls each individual name's returns.
+	pull_ticker_info <- function(ticker, from, to) {
+		tryCatch(ts <- cbind(yahooSeries(ticker, from, to)), error=function(ex) return(NULL))
+
+		if(class(ts) != "timeSeries") return(NULL)
+		
+		colnames(ts) <- c("Open","High","Low","Close","Volume","Adj.Close")
+		
+		#Compute next day's returns.
+		df <- as.data.frame(cbind(ts, Returns=lag(returns(ts[,'Close']), -1)))
+		
+		df$Day <- as.Date(rownames(df))
+		df$Ticker <- ticker
+		df
+	}
+	
+	#Merge the reseults together and return. The doparallel option will speed this up, but only
+	#on MacOS or Linux.
+	if(doparallel) {
+	    require(doMC)
+	    registerDoMC(nprocs)
+	    df <- foreach(t=tickerlist, .combine='rbind') %dopar% pull_ticker_info(t, from=from, to=to)
+	}
+	else { 
+	    df <- do.call("rbind", lapply(tickerlist, FUN=pull_ticker_info, from=from, to=to))
+	}
+	
+	rownames(df) <- NULL
+	df
+}
+
+#This function will be used to compute deciles later on. It is not robust to low-variance data.
+decile <- function(x) { cut(x,quantile(x,(0:10)/10,na.rm=TRUE),labels=FALSE,include.lowest=TRUE) }
+
+#Expects a data.frame with a column called "Day", one called "Decile", and one called "Returns"
+plot_decile_performance <- function(df, benchmark) {
+	
+	#Compute aggregate returns by decile.
+	agg <- with(df, aggregate(list(Returns=Returns), by=list(Day=Day,Decile=Decile), FUN=mean, na.rm=TRUE))
+	perfdf <- cast(agg, Day ~ Decile, value='Returns')
+    perfdf <- as.data.frame(perfdf)
+	rownames(perfdf) <- perfdf[,1]
+	perfdf <- perfdf[,-1]
+
+    #Convert and add in benchmark returns.
+    rownames(benchmark) <- benchmark$Day
+    benchname <- benchmark$Ticker[1]
+    benchmark <- benchmark[,'Returns',drop=F]
+    colnames(benchmark) <- benchname
+    
+    colnames(perfdf) <- paste("Decile", colnames(perfdf))
+    
+    perfdf <- cbind(perfdf, benchmark)
+	
+	chart.CumReturns(perfdf, legend.loc="topleft", main="Performance of Recorded Future Factor Deciles", colorset=rich12equal)
+}
+
+
+#Load up tickers to run against.
+tickerlist <- scan(tickerfile, what='character')
+
+#Pull market data for ticker list and date range.
+market_data <- pull_market_data(tickerlist, from, to)
+bench_data <- pull_market_data(list("SPY"), from, to)
+
+#Pull Recorded Future data for ticker list and date range.
+rf_data <- run_agg_query_by_ticker(tickerlist, from, to, token)
+
+#Subset the Recorded Future Data to include only days I'm interested in.
+rf_data <- with(rf_data, rf_data[Positive - Negative > 0,])
+
+#You can compute your own metrics on the Recorded Future data here.
+rf_data$logCount <- log(rf_data$Count)
+
+#Merge the Recorded Future Data with the Market Data
+merged_data <- merge(rf_data, market_data, by=c("Ticker","Day"))
+
+#Calculate deciles of a variable of interest.
+merged_data <- ddply(merged_data, .(Day), transform, Decile=decile(Momentum))
+
+#Produce a plot of decile performance.
+plot_decile_performance(merged_data, bench_data)
+
+
---
+++ R-examples/factorDeciles/rfapi.R
@@ -0,0 +1,135 @@
+library(rjson)
+library(RCurl)
+library(plyr)
+
+url <- 'http://api.recordedfuture.com/ws/rfq/instances'
+
+aggregateQuery<-'{
+ "aggregate_raw": {
+   "document": {"published": {"min": "2009-01-01", "max": "2009-09-30"}}
+ },
+ "output": {
+   "fields": ["momentum", "count", "positive", "negative"],
+   "format": "csv"
+ }
+}'
+
+aggregateSourceQuery<-'{
+ "aggregate_raw": {
+   "document": {"published": {"min": "2009-01-01", "max": "2009-09-30"}}
+ },
+ "output": {
+   "fields": ["source","momentum", "count", "positive", "negative"],
+   "format": "csv"
+ }
+}'
+
+entityQuery<-'{
+    "entity": {
+        "type": "Company",
+        "attributes": {
+            "name": "tickers"
+        }
+    },
+    "output": {
+        "fields":["id","attributes"],
+        "entity_details":{"Company": ["gics","tickers"]}
+    }
+}'
+
+
+load_query <- function(filename) {
+	f <- file(filename, "r")
+	lines <- paste(readLines(f), collapse='\n')
+	close(f)
+	
+	fromJSON(lines)
+}
+
+run_agg_query <- function(query, token, ids, from, to) {
+    query <- fromJSON(query)
+    query$token <- token
+    
+    #Set query parameters
+    query$aggregate_raw$document$published$min <- from
+    query$aggregate_raw$document$published$max <- to
+    query$aggregate_raw$entity$id = as.integer(as.character(ids))
+    
+	opts <- curlOptions(header = FALSE, connecttimeout = 0)
+	
+	res <- postForm(url,q=toJSON(query), style="post", .opts = opts)
+
+	res <- fromJSON(res)
+	
+	if(res$status == "SUCCESS") {
+		con <- textConnection(res$aggregates, "r")
+		agg <- read.csv(con)
+		close(con)
+		
+        #Switch up data types on columns from defaults.
+        agg$Day <- as.Date(as.character(agg$Day))
+        agg$Entity <- as.character(agg$Entity)
+		
+		return(agg)
+	}
+
+	res
+}
+
+getTickerId<-function(ticker, token) {
+    query<-fromJSON(entityQuery)
+    query$token<-token
+    query$entity$attributes$string<-ticker
+    res<-run_query(query)
+    
+    res$entities
+}
+
+
+run_query <- function(q) {
+	opts <- curlOptions(header = FALSE, connecttimeout = 0)
+
+	res <- postForm(url,q=toJSON(q), style="post", .opts = opts)
+	res <- fromJSON(res)
+
+	res
+}
+
+ticker_lookup <- function(tickers, token) {
+    query <- fromJSON(entityQuery)
+    query$token <- token
+    query$entity$attributes$string <- tickers
+    res <- run_query(query)
+    
+    if(res$status == "SUCCESS") {
+        ed <- res$entity_details
+        
+        #Convert the results to a data.frame
+        entity_table <- as.data.frame(do.call("rbind", mapply(function(y, x) cbind(y, x$tickers, x$momentum), names(ed), ed)))
+        colnames(entity_table) <- c("Entity","Ticker","Momentum")
+        entity_table$Momentum <- as.numeric(as.character(entity_table$Momentum))
+        
+        #This function filters by top momentum for each entity.
+        #Idea being the entity with highest momentum is most likely the "correct" one.
+        entity_table <- ddply(entity_table, .(Ticker), function(x) x[x$Momentum == max(x$Momentum),][1])
+
+        return(entity_table[entity_table$Ticker %in% tickers,])
+    }
+    res
+}
+
+
+run_agg_query_by_ticker <- function(tickers, from, to, token) {
+    rf_tickermap <- ticker_lookup(tickers, token)
+
+    #Execute query and merge in ticker info.
+    resdf <- run_agg_query(aggregateQuery, token, rf_tickermap$Entity, from, to)
+    if(class(resdf) == "data.frame") {
+        compdf <- merge(rf_tickermap[,c('Entity','Ticker')], resdf, by='Entity')
+        compdf$Ticker <- as.character(compdf$Ticker)
+    
+        return(with(compdf, compdf[order(Ticker, Day),]))
+    }
+    resdf
+}
+
